27ba2be47005b0ea0df077d4526cb8cced4237d3
mauromussin <mauro.mussin@gmail.com>
2018-04-13 14:12:22 +0200

    prima versione

---
+++ README.md
@@ -0,0 +1,9 @@
+# feed IRIS
+Alimentazione di IRIS tramite script python in sostituzione di recupero_RT
+
+# prerequisiti
+1. rwmwsgwyd funzionante
+2. tabelle di IRIS per il temporeale su postgres
+3. pacchetti di python indicati nel codice
+
+# funzionamento
---
+++ feed_iris.py
@@ -0,0 +1,120 @@
+# Alimentazione di IRIS
+# logica di funzionamento: il programma legge le variabili di ambiente e può eseguire l'alimentazione in diverse modalità
+# a. diretta: non ci sono ancora dati e vengono acquisiti direttamente dal REM (datainizio e datafine coincidono)
+# b. in recupero: possono esserci delle lacune che vengono identificate e il dato corrispondente richiesto al REMWS_GATEWAY
+#    in questo caso il recupero va da datainizio a datafine
+# 1. inizializzazione
+# 2. richiesta elenco sensori al db (df_sensori)
+
+# FASE 1
+#inizializzazione
+import os
+import pandas as pd
+import numpy as np
+from sqlalchemy import *
+import datetime as dt
+import json as js
+import requests
+# variabili di ambiente (da togliere in produzione)
+IRIS_USER_ID='postgres'
+IRIS_USER_PWD='p0stgr3S'
+IRIS_DB_NAME='iris_base'
+IRIS_DB_HOST='10.10.0.19'
+REMWS_GATEWAY='http://10.10.0.15:9099'
+url=REMWS_GATEWAY
+DEBUG=False
+IRIS_TABLE_NAME='m_osservazioni_tr'
+IRIS_SCHEMA_NAME='realtime'
+AUTORE=os.getenv('COMPUTERNAME')
+MINUTES=120 # minuti di recupero
+TIPOLOGIE=['T'] # elenco delle tipologie da cercare nella tabella delle osservazioni realtime, è una lista
+# inizializzazione delle date
+datafine=dt.datetime.now()
+datainizio=datafine-dt.timedelta(minutes=MINUTES)
+#definizione delle funzioni
+def seleziona_richiesta(Risposta):
+    # definisco dataframe della risposta
+    df_risposta=pd.DataFrame(columns=['data_e_ora','misura','validita'])
+    # dizionario di appoggio con la selezione dei dati
+    aa=Risposta['data']['sensor_data_list'][0]['data']
+    # ciclo lettura
+    for i in range(1,len(aa)-1):
+    #    print(i,aa[i]['datarow'].split(";")[2])
+        df_risposta.loc[i-1]=[aa[i]['datarow'].split(";")[0],aa[i]['datarow'].split(";")[1],aa[i]['datarow'].split(";")[2]]
+    return df_risposta
+#FASE 2 - query al dB
+engine = create_engine('postgresql+pg8000://'+IRIS_USER_ID+':'+IRIS_USER_PWD+'@'+IRIS_DB_HOST+'/'+IRIS_DB_NAME)
+conn=engine.connect()
+
+#preparazione dell'elenco dei sensori
+Query='Select *  from "dati_di_base"."anagraficasensori" where "anagraficasensori"."datafine" is NULL and idrete in (1,4);'
+df_sensori=pd.read_sql(Query, conn)
+# preparazione dei dati
+#QueryJoin='Select "m_osservazioni_tr"."idsensore", "m_osservazioni_tr"."data_e_ora","m_osservazioni_tr"."misura" from "realtime"."m_osservazioni_tr" inner join "dati_di_base"."anagraficasensori" on ("m_osservazioni_tr"."idsensore"="anagraficasensori"."idsensore") where "anagraficasensori"."datafine" is NULL and "m_osservazioni_tr"."data_e_ora" between \''+datainizio.strftime("%Y-%m-%d %H:%M")+'\' and \''+datafine.strftime("%Y-%m-%d %H:%M")+'\';'
+#ALIMETAZIONE DIRETTA
+# suppongo di non avere ancora chisto dati, vedo qule dato devo chiedere, lo chiedo e loinserisco.
+# Se l'inserimento fallisce vuol dire che qualcun altro ha inserito il dato (ovvero un processo in parallelo, il che èstrano...)
+
+# selezione dell'ora
+minuto=int(datainizio.minute/10)*10
+data_ricerca=dt.datetime(datainizio.year,datainizio.month,datainizio.day,datainizio.hour,minuto,0)
+
+
+#ciclo sui sensori:
+# strutturo la richiesta
+id_operatore=1
+function=1
+frame_dati={}
+frame_dati["sensor_id"]=0
+frame_dati["granularity"]=1 # chiedo solo i 10 minuti
+frame_dati["start"]=data_ricerca.strftime("%Y-%m-%d %H:%M")
+frame_dati["finish"]=data_ricerca.strftime("%Y-%m-%d %H:%M")
+#suppongo che in df_section ci siano solo i sensori che mi interessano e faccio il ciclo di richiesta
+s=dt.datetime.now()
+conn=engine.connect()
+
+
+for row in df_section.itertuples():
+    frame_dati["sensor_id"]=row.idsensore
+    # assegno operatore e funzione corretti
+    if(row.nometipologia=='PP' or row.nometipologia=='N'):
+            id_operatore=4
+            function=3
+    else:
+            id_operatore=1
+            function=1
+    frame_dati["operator_id"]=id_operatore
+    frame_dati["function_id"]=function
+    richiesta={
+        'header':{'id': 10},
+        'data':{'sensors_list':[frame_dati]}
+        }
+    #print (richiesta)
+    try:
+        r=requests.post(url,data=js.dumps(richiesta))
+    except:
+        print("Errore: REMWS non raggiungibile", end="\r")
+    risposta=js.loads(r.text)
+    outcome=risposta['data']['outcome']
+    if(outcome==0):
+        # estraggo il dato
+        aa=risposta['data']['sensor_data_list'][0]['data']
+        #se contiene almeno tre elementi c'è anche il dato
+        if (len(aa)>2):
+            misura=aa[1]['datarow'].split(";")[1]
+            #print(row.idsensore, misura)
+            QueryInsert='INSERT into "'+IRIS_SCHEMA_NAME+'"."'+IRIS_TABLE_NAME+\
+            '" (idsensore,nometipologia,idoperatore,data_e_ora,misura, autore,data)\
+            VALUES ('+str(row.idsensore)+',\''+row.nometipologia+'\','+str(id_operatore)+',\''+\
+            data_ricerca.strftime("%Y-%m-%d %H:%M")+'\','+str(misura)+',\''+ AUTORE+'\',\''+\
+            s.strftime("%Y-%m-%d %H:%M")+'\');'
+            print(QueryInsert)
+            try:
+                conn.execute(QueryInsert)
+
+            except:
+                print("Query non riuscita!")
+
+    else:
+        print ("Attenzione: dato di ",TIPOLOGIE, "sensore ", row.idsensore, "ASSENTE nel REM")
+print(s,dt.datetime.now())
