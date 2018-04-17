5e2d926a5bb6c36b4a9f60be391aa54c9e2aa447
root <root@TESTV-DCKRSWRM1.arpa.local>
2018-04-17 13:26:24 +0000

    modifica al launcher

--- launch_feed.sh
+++ launch_feed.sh
@@ -1,5 +1,22 @@
-!#bin/bash
-# lancio del programma python
-# 1. verifico se sono al minuto n,n+10,n+20,n+30,n+40,n+50
-# si-> eseguo
-# no-> aspetto
+#!/bin/bash
+#
+# lo script è all'interno del container e lancia in sequenza lo script R ogni 10 minuti
+# lo script fa sempre recuperi corti ogni 20'
+#source ~/.bash_profile;
+#  s/S = short, ovvero recupero 1h
+#  l/L = long, ovvero recupero 24h
+#controllo se il container è già stato lanciato: se sì, ritardo l'esecuzione 
+# lo inserisco in un ciclo di attesa di 10 minuti, trascorsi i quali non eseguo il comando
+nomescript=${0##*/}
+while [ 1 ]
+do
+   data_corrente=$[ 10#$(date +"%M") % 10 ]
+   if [ $data_corrente == 3  ]
+   then
+      echo 'lancio script'
+      python3 feed_iris.py
+   else
+     sleep 10
+     echo $data_corrente
+   fi
+done
