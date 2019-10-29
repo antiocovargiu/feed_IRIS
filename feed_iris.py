e21f98c90f8533ad0332fddf518e4a123f95d7b3
mauromussin <m.mussin@arpalombardia.it>
2019-10-29 13:46:47 +0100

    corretto per df_section

--- feed_iris.py
+++ feed_iris.py
@@ -103,8 +103,7 @@ conn=engine.connect()
 #preparazione dell'elenco dei sensori
 Query='Select *  from "dati_di_base"."anagraficasensori" where "anagraficasensori"."datafine" is NULL and idrete in (1,2,4);'
 df_sensori=pd.read_sql(Query, conn)
-# sort casuale per parallelizzazione
-df_sensori.sample(frac=1)
+
 
 #ALIMETAZIONE DIRETTA
 # suppongo di non avere ancora chisto dati, vedo qule dato devo chiedere, lo chiedo e loinserisco.
@@ -115,6 +114,8 @@ minuto=int(datainizio.minute/10)*10
 data_ricerca=dt.datetime(datainizio.year,datainizio.month,datainizio.day,datainizio.hour,minuto,0)
 ora=dt.datetime(datainizio.year,datainizio.month,datainizio.day,datainizio.hour,0,0)
 df_section=df_sensori[df_sensori.nometipologia.isin(TIPOLOGIE)]
+# sort casuale per parallelizzazione
+df_section.sample(frac=1)
 #ciclo sui sensori:
 # strutturo la richiesta
 id_operatore=1
