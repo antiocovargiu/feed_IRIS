c20a570d7f0d076788b5bec1f5ef66c93a3224dd
mauromussin <m.mussin@arpalombardia.it>
2019-11-11 13:37:17 +0100

    Aggiunta variabile x timeout

--- feed_iris_recupero.py
+++ feed_iris_recupero.py
@@ -32,6 +32,7 @@ if (AUTORE==None):
     DEBUG=os.getenv('DEBUG')
     MINUTES=int(os.getenv('MINUTES'))
     REMWS_GATEWAY=os.getenv('REMWS_GATEWAY')
+    TIMEOUT=os.getenv('TIMEOUT')
     # trasformo la stringa in lista
 
 url=REMWS_GATEWAY
@@ -71,7 +72,7 @@ def Richiesta_remwsgwy (framedati):
         }
     ci_sono_dati=False
     try:
-        r=requests.post(url,data=js.dumps(richiesta),timeout=1)
+        r=requests.post(url,data=js.dumps(richiesta),timeout=TIMEOUT)
         if(len(r.text)>0):
                 risposta=js.loads(r.text)
                 #controllo progressivamente se la risposta è buona e se ci sono dati
